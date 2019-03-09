c763e314add7b06db060c240429eac262a589981
root <root@ts2.example.org>
2019-03-09 17:10:31 -0600

    mostly fleshing out functionalility test.py now has examples for most useful functions

--- libmicon.py
+++ libmicon.py
@@ -9,15 +9,97 @@ retry_count = 2
 
 CMD_MODE_READ		= 0x80
 
-CMD_OPERATION_SOUND	= 0x30
-DT_BZ_STOP	= 0x00
-DT_BZ_MACHINE	= 0x01
-DT_BZ_BUTTON	= 0x02
-DT_BZ_CONTINUE	= 0x03
-DT_BZ_SWITCH1	= 0x04
-DT_BZ_SWITCH2	= 0x10
-DT_BZ_MUSIC1	= 0x30
-DT_BZ_MUSIC2	= 0x20
+####Commands (not all commands supported by all versions)
+bz_on				= 0x30
+lcd_set_linkspeed		= 0x31
+lcd_set_dispitem		= 0x32
+fan_set_speed			= 0x33
+system_get_mode			= 0x34
+system_set_watchdog		= 0x35
+int_get_switch_status		= 0x36
+fan_get_speed			= 0x38
+lcd_get_dispplane		= 0x39
+lcd_set_bright			= 0x3a
+usb_set_power			= 0x3e
+lcd_set_dispitem_ex		= 0x40
+power_state			= 0x46
+led_set_cpu_mcon		= 0x50
+bz_set_freq			= 0x53
+fan_get_speed_ex		= 0x57
+sata_led_set_on_off		= 0x58
+sata_led_set_blink		= 0x59
+lcd_set_disk_capacity		= 0x60
+lcd_set_date			= 0x70
+lcd_set_hostname		= 0x80
+lcd_set_ipaddress		= 0x81
+lcd_set_raidmode		= 0x82
+mcon_get_version		= 0x83
+mcon_get_taskdump		= 0x84
+boot_device_error		= 0x93
+lcd_set_raidmode32		= 0x94
+lcd_set_buffer0			= 0xa0
+lcd_set_buffer1			= 0xa1
+lcd_set_buffer2			= 0xa2
+lcd_set_buffer3			= 0xa3
+lcd_set_buffer4			= 0xa4
+lcd_set_buffer5			= 0xa5
+lcd_set_buffer6			= 0xa6
+lcd_set_buffer7			= 0xa7
+lcd_init			= 0x01
+boot_start			= 0x02
+boot_end			= 0x03
+boot_dramdt_ng			= 0x04
+boot_dramad_ng			= 0x05
+power_off			= 0x06
+boot_flash_ok			= 0x07
+boot_dram_ok			= 0x08
+lcd_disp_animation		= 0x09
+boot_error_del			= 0x0b
+shutdown_wait			= 0x0c
+shutdown_cancel			= 0x0d
+reboot				= 0x0e
+serialmode_console		= 0x0f
+serialmode_ups			= 0x10
+ups_linefail_off		= 0x11
+ups_linefail_on			= 0x12
+ups_vender_apc			= 0x13
+ups_vender_omron		= 0x14
+lcd_disp_temp_on		= 0x15
+lcd_disp_temp_off		= 0x16
+lcd_changemode_button		= 0x17
+lcd_changemode_auto		= 0x18
+ups_shutdown_off		= 0x19
+ups_shutdown_on			= 0x1a
+lcd_disp_fanspeed_on		= 0x1b
+lcd_disp_fanspeed_off		= 0x1c
+bz_disp_on			= 0x1d
+bz_disp_off			= 0x1e
+lcd_disp_linkspeed		= 0x20
+lcd_disp_hostname		= 0x21
+lcd_disp_diskcap		= 0x22
+lcd_disp_raidmode		= 0x23
+lcd_disp_date			= 0x24
+lcd_disp_buffer0		= 0x25
+lcd_disp_buffer1		= 0x26
+lcd_disp_buffer2		= 0x27
+shutdown_ups_recover		= 0x28
+wol_ready_on			= 0x29
+wol_ready_off			= 0x2a
+lcd_disp_buffer3		= 0x2b
+lcd_disp_buffer4		= 0x2c
+lcd_disp_buffer5		= 0x2d
+lcd_disp_buffer6		= 0x2e
+lcd_disp_buffer7		= 0x2f
+
+
+BZ_STOP	= 0x00
+BZ_MACHINE	= 0x01
+BZ_BUTTON	= 0x02
+BZ_CONTINUE	= 0x03
+BZ_SWITCH1	= 0x04
+BZ_SWITCH2	= 0x10
+BZ_MUSIC1	= 0x30
+BZ_MUSIC2	= 0x20
 
 LED_ON		= 1
 LED_OFF		= 0
@@ -39,10 +121,10 @@ DISP_LED	= [0x00,0x80]
 SATA_LED_ONOFF	= 0x58
 SATA_LED_BLINK	= 0x59
 
-SATA_ALL	= [0xFF,0xFF] ##works for off but not on? 
+SATA_ALL_LED	= [0xFF,0xFF] ##works for off but not on? 
 SATA_ALL_GREEN	= [0x00,0xFF]
 SATA_ALL_RED	= [0xFF,0x00]
-
+SATA_ALL_RED  	= [0xFF,0x00]
 SATA1_RED	= [0x01,0x00]
 SATA2_RED	= [0x02,0x00]
 SATA3_RED       = [0x04,0x00]
@@ -52,39 +134,42 @@ SATA6_RED       = [0x20,0x00]
 SATA7_RED       = [0x40,0x00]
 SATA8_RED       = [0x80,0x00]
 
-SATA1_GREEN	= [0x00,0x01]
-SATA2_GREEN     = [0x00,0x02]
-SATA3_GREEN     = [0x00,0x04]
-SATA4_GREEN     = [0x00,0x08]
-SATA5_GREEN     = [0x00,0x10]
-SATA6_GREEN     = [0x00,0x20]
-SATA7_GREEN     = [0x00,0x40]
-SATA8_GREEN     = [0x00,0x80]
+SATA_ALL_GREEN		= [0x00,0xFF]
+SATA1_GREEN		= [0x00,0x01]
+SATA2_GREEN     	= [0x00,0x02]
+SATA3_GREEN     	= [0x00,0x04]
+SATA4_GREEN     	= [0x00,0x08]
+SATA5_GREEN     	= [0x00,0x10]
+SATA6_GREEN     	= [0x00,0x20]
+SATA7_GREEN     	= [0x00,0x40]
+SATA8_GREEN     	= [0x00,0x80]
 
-CMD_LCD_BRIGHTNESS	= 0x3A
+LCD_BRIGHT_FULL	= 0xFF
+LCD_BRIGHT_LOW	= 0x77
+LCD_BRIGHT_MED  = 0xCC
+LCD_BRIGHT_OFF  = 0x00
 
-CMD_LCD_LINK		= 0x20  ##0x31 0x00 - 0x05
-CMD_LCD_DISK_USAGE	= 0x21  ## 4 bars controlled by bytes of 0x60 (bar height 00-0F, toggle up arrow with 1_)
-CMD_LCD_IPADDRESS	= 0x22  ##write: top=0x80 bottom=0x81
-CMD_LCD_OPMODE		= 0x23  ##write via 0x94; just top row via 0x82?
-CMD_LCD_DATE		= 0x24  ##timer which can be updated at 0x70
-CMD_LCD_BUFFER0		= 0x25  ##write via 0xA5 or 0x93 or 0x90?
-CMD_LCD_BUFFER1         = 0x26  ##write via 0xA5 or 0x91?
-CMD_LCD_BUFFER2         = 0x27  ##write via 0xA7 or 0x92?
-CMD_LCD_BUFFER3         = 0x2B
-CMD_LCD_BUFFER4         = 0x2C
-CMD_LCD_BUFFER5         = 0x2D
-CMD_LCD_BUFFER6         = 0x2E
-CMD_LCD_BUFFER7         = 0x2F
+LCD_COLOR_RED 		= [0x00,0x01]
+LCD_COLOR_GREEN		= [0x00,0x02]
+LCD_COLOR_ORANGE	= [0x00,0x03]
+LCD_COLOR_BLUE		= [0x00,0x04]
+LCD_COLOR_PURPLE	= [0x00,0x05]
+LCD_COLOR_AQUA		= [0x00,0x06]
+LCD_COLOR_MAGENTA	= [0x00,0x07]
 
-## 0x00 = "No Link"
-## 0x01 = "10Mbps HALF"
-## 0x02 = "10Mbps FULL"
-## 0x03 = "100Mbps HALF"
-## 0x04 = "100Mbps FULL"
-## 0x05 = "1000Mbps"
+GRAPH_0p	= 0x00
+GRAPH_25p	= 0x04
+GRAPH_50p	= 0x08
+GRAPH_75p	= 0x0C
+GRAPH_100p	= 0x0F
+GRAPH_ARROW	= 0x10
 
-READ_ONLY_MODEL_STRING  = 0x83
+LINK_NOLINK	= 0x00
+LINK_10M_HALF	= 0x01
+LINK_10M_FULL	= 0x02
+LINK_100M_HALF	= 0x03
+LINK_100M_FULL	= 0x04
+LINK_1000M	= 0x05
 
 
 class micon_api:
@@ -116,14 +201,11 @@ class micon_api:
 		self.port.write(output)
 
 	def send_preamble(self):
-		bytelist= [0xFF]*32
-		preamble = bytearray(bytelist)
+		preamble = bytearray([0xFF]*32)
 		self.send_bytes(preamble)
 
 	def send_cmd(self, cmdbytes):
 		response = 0xFF
-		if type(cmdbytes) == "int":
-			cmdbytes = bytearray([cmdbytes])
 		cmdbytes.append(self.calc_check_byte(cmdbytes))
 		print ("sending : ", bytes(cmdbytes).hex()," ",self.calc_checksum(cmdbytes)," ...", end="")
 		for x in range(retry_count):
@@ -155,10 +237,10 @@ class micon_api:
 		print ("response: ", bytes(output).hex())
 		return output
 
-	def send_write_cmd(self, length, addrbyte, databytes):
+	def send_write_cmd(self, length, addrbyte, databytes=0x00):
 		#validate length or perhaps stop requiring it or something
 		if type(databytes) == int:
-                	databytes = bytearray([databytes])
+                        databytes = bytearray([databytes])
 		cmdbytes=bytearray()
 		cmdbytes.append(length)
 		cmdbytes.append(addrbyte)
@@ -174,7 +256,7 @@ class micon_api:
 		return response
 
 	def cmd_sound(self, soundcmd):
-		return self.send_write_cmd(1, CMD_OPERATION_SOUND, soundcmd)
+		return self.send_write_cmd(1, bz_on, soundcmd)
 
 	def cmd_set_sataled(self, mode, led):
 		led_mask = bytearray(led)
@@ -228,12 +310,15 @@ class micon_api:
 		self.send_write_cmd(2,CMD_LED_ONOFF,led_status)
 		#send_write_cmd(port,2,CMD_LED_CPU,inv_mask)
 
-	def set_lcd_brightness(self, led_byte, lcd_byte):
-		cmdbytes=bytearray([led_byte | lcd_byte])
-		self.send_write_cmd(1,CMD_LCD_BRIGHTNESS,cmdbytes)
+	def set_lcd_brightness(self, brightbyte):
+		self.send_write_cmd(1,lcd_set_bright,brightbyte)
 
 	def get_lcd_brightness(self):
-        	self.send_read_cmd(CMD_LCD_BRIGHTNESS)
+        	self.send_read_cmd(lcd_set_bright)
+
+	def set_lcd_color(self, colorbytes):
+		self.send_write_cmd(2,CMD_LED_CPU,bytearray([0x00,0x07]))
+		self.send_write_cmd(2,CMD_LED_ONOFF,colorbytes)
 
 	#both unsure what this does, exactly and whether it's right
 	def cmd_set_lcddisp(self, mode, led):
@@ -266,7 +351,7 @@ class micon_api:
 		messagebytes.extend(map(ord,message))
 		self.send_write_cmd(32, bufferbyte,messagebytes)
 
-	def set_lcd_buffer2(self, bufferbyte, message):
+	def set_lcd_buffer_short(self, bufferbyte, message):
         	message = message.ljust(16)
         	message = message[:16]
         	messagebytes = bytearray()
@@ -284,5 +369,7 @@ class micon_api:
 		min=int(currentDT.strftime('%M'))
 		sec=int(currentDT.strftime('%S'))
 		output=bytearray([year,mon,day,hour,min,sec])
-		self.send_write_cmd(6,0x70,output)
+		self.send_write_cmd(6,lcd_set_date,output)
 
+	def set_lcd_drives(self, drive1, drive2, drive3, drive4):
+		self.send_write_cmd(4,lcd_set_disk_capacity,bytearray([drive1,drive2,drive3,drive4]))
--- test.py
+++ test.py
@@ -1,27 +1,89 @@
 #!/usr/bin/python3
 
 import libmicon
+import socket
+import time
 
 test = libmicon.micon_api()
 
+##update the date for the lcd display
 test.set_lcd_date()
+test.cmd_force_lcd_disp(libmicon.lcd_disp_date)
+time.sleep(1)
+
+##set the ip address for the lcd display
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('10.255.255.255', 1))
+host_ip = s.getsockname()[0]
+s.close()
+host_name = socket.gethostname()
+test.set_lcd_buffer_short(libmicon.lcd_set_ipaddress,host_ip)
+test.set_lcd_buffer_short(libmicon.lcd_set_hostname,host_name)
+
+test.cmd_force_lcd_disp(libmicon.lcd_disp_hostname)
+time.sleep(1)
+
+##set link speed (just to gbps)
+test.send_write_cmd(1,libmicon.lcd_set_linkspeed,libmicon.LINK_1000M)
+test.send_write_cmd(1,0x37,libmicon.LINK_1000M)
+test.cmd_force_lcd_disp(libmicon.lcd_disp_linkspeed)
+time.sleep(1)
+
+##set drive capacity graphs with bogus values
+test.set_lcd_drives(libmicon.GRAPH_25p,libmicon.GRAPH_50p,libmicon.GRAPH_75p,libmicon.GRAPH_100p)
+test.cmd_force_lcd_disp(libmicon.lcd_disp_diskcap)
+time.sleep(1)
+
 ##set custom lcd message
-test.set_lcd_buffer(0xA0,"Terastation x86","   Debian 9.8")
+test.set_lcd_buffer(libmicon.lcd_set_buffer0,"Terastation x86","   Debian 9.8")
+test.cmd_force_lcd_disp(libmicon.lcd_disp_buffer0)
 
-#blank turn off all LEDs
-test.cmd_set_led(libmicon.LED_OFF, [0xFF,0x0F] )
+##restrict available displays to just our custome message for now
+##still working on how to switch between auto and manual cycling of displays.
+test.send_write_cmd(1,libmicon.lcd_set_dispitem,0x20)
+test.send_write_cmd(1,libmicon.lcd_set_dispitem_ex,0x00)
 
-#enable power led and link led, set backlight as blue+green
-test.cmd_set_led(libmicon.LED_ON, [0x29,0x06] )
+#these don't seem to work, I've had it in both modes at some point but am unclear how to switch.
+#test.send_write_cmd(0,libmicon.lcd_changemode_button)
+#test.send_write_cmd(0,lcd_changemode_auto)
 
-#set lcd backlight to maximum
-test.set_lcd_brightness(0xF0, 0x0F)
+##turn off all sata leds then cycle through them
+test.cmd_set_sataled(libmicon.LED_OFF,libmicon.SATA_ALL_LED)
 
-#display custom lcd message
-test.cmd_force_lcd_disp(0x25)
+for led in [libmicon.SATA1_RED, libmicon.SATA2_RED, libmicon.SATA3_RED, libmicon.SATA4_RED,libmicon.SATA5_RED, libmicon.SATA6_RED, libmicon.SATA7_RED, libmicon.SATA8_RED]:
+	test.cmd_set_sataled(libmicon.LED_ON,led)
 
-#play a sound!
-test.cmd_sound(0x20)
+time.sleep(0.5)
+test.cmd_set_sataled(libmicon.LED_OFF,libmicon.SATA_ALL_RED)
+
+for led in [libmicon.SATA1_GREEN, libmicon.SATA2_GREEN, libmicon.SATA3_GREEN, libmicon.SATA4_GREEN,libmicon.SATA5_GREEN, libmicon.SATA6_GREEN, libmicon.SATA7_GREEN, libmicon.SATA8_GREEN]:
+        test.cmd_set_sataled(libmicon.LED_ON,led)
+
+
+#turn off all LED then cycle through turning them on.
+test.cmd_set_led(libmicon.LED_OFF,[0xFF,0x00])
+
+for led in [libmicon.POWER_LED, libmicon.INFO_LED, libmicon.ERROR_LED, libmicon.LAN1_LED, libmicon.LAN2_LED, libmicon.FUNC1_LED, libmicon.FUNC2_LED]:
+	test.cmd_set_led(libmicon.LED_OFF,led)
+	test.cmd_set_led(libmicon.LED_ON,led)
+	time.sleep(0.25)
+	test.cmd_set_led(libmicon.LED_OFF,led)
+
+#cycle through all the backlight combinations
+for color in [libmicon.LCD_COLOR_RED, libmicon.LCD_COLOR_BLUE, libmicon.LCD_COLOR_GREEN, libmicon.LCD_COLOR_ORANGE, libmicon.LCD_COLOR_AQUA]:
+	test.set_lcd_color(color)
+	time.sleep(0.5)
+
+#set LCD backlight brightness
+for level in [libmicon.LCD_BRIGHT_OFF,libmicon.LCD_BRIGHT_LOW,libmicon.LCD_BRIGHT_MED,libmicon.LCD_BRIGHT_FULL]:
+	test.set_lcd_brightness(level)
+	time.sleep(0.25)
+
+#run some standalone commands
+#test.send_write_cmd(0,libmicon.lcd_disp_animation)
+
+#try every possible sound setting.
+test.cmd_sound(libmicon.BZ_MUSIC2)
 
 test.port.close()
 quit()
