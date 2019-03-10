9d5b7f0bc3ffd35ae340e7cf049958aca0cd7bf4
root <root@ts2.example.org>
2019-03-10 12:23:45 -0500

    added pauses back into test script

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ test = libmicon.micon_api()
 ##update the date for the lcd display
 test.set_lcd_date()
 test.cmd_force_lcd_disp(libmicon.lcd_disp_date)
-#time.sleep(1)
+time.sleep(1)
 
 ##set the ip address for the lcd display
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,18 +21,18 @@ test.set_lcd_buffer_short(libmicon.lcd_set_ipaddress,host_ip)
 test.set_lcd_buffer_short(libmicon.lcd_set_hostname,host_name)
 
 test.cmd_force_lcd_disp(libmicon.lcd_disp_hostname)
-#time.sleep(1)
+time.sleep(1)
 
 ##set link speed (just to gbps)
 test.send_write_cmd(1,libmicon.lcd_set_linkspeed,libmicon.LINK_1000M)
 test.send_write_cmd(1,0x37,libmicon.LINK_1000M)
 test.cmd_force_lcd_disp(libmicon.lcd_disp_linkspeed)
-#time.sleep(1)
+time.sleep(1)
 
 ##set drive capacity graphs with bogus values
 test.set_lcd_drives(libmicon.GRAPH_25p,libmicon.GRAPH_50p,libmicon.GRAPH_75p,libmicon.GRAPH_100p)
 test.cmd_force_lcd_disp(libmicon.lcd_disp_diskcap)
-#time.sleep(1)
+time.sleep(1)
 
 ##set custom lcd message
 test.set_lcd_buffer(libmicon.lcd_set_buffer0,"Terastation x86","   Debian 9.8")
@@ -54,7 +54,7 @@ test.cmd_set_sataled(libmicon.LED_OFF,libmicon.SATA_ALL_LED)
 for led in [libmicon.SATA1_RED, libmicon.SATA2_RED, libmicon.SATA3_RED, libmicon.SATA4_RED,libmicon.SATA5_RED, libmicon.SATA6_RED, libmicon.SATA7_RED, libmicon.SATA8_RED]:
 	test.cmd_set_sataled(libmicon.LED_ON,led)
 
-#time.sleep(0.5)
+time.sleep(0.5)
 test.cmd_set_sataled(libmicon.LED_OFF,libmicon.SATA_ALL_RED)
 
 for led in [libmicon.SATA1_GREEN, libmicon.SATA2_GREEN, libmicon.SATA3_GREEN, libmicon.SATA4_GREEN,libmicon.SATA5_GREEN, libmicon.SATA6_GREEN, libmicon.SATA7_GREEN, libmicon.SATA8_GREEN]:
@@ -67,18 +67,18 @@ test.cmd_set_led(libmicon.LED_OFF,[0xFF,0x00])
 for led in [libmicon.POWER_LED, libmicon.INFO_LED, libmicon.ERROR_LED, libmicon.LAN1_LED, libmicon.LAN2_LED, libmicon.FUNC1_LED, libmicon.FUNC2_LED]:
 	test.cmd_set_led(libmicon.LED_OFF,led)
 	test.cmd_set_led(libmicon.LED_ON,led)
-#	time.sleep(0.25)
+	time.sleep(0.25)
 	test.cmd_set_led(libmicon.LED_OFF,led)
 
 #cycle through all the backlight combinations
 for color in [libmicon.LCD_COLOR_RED, libmicon.LCD_COLOR_BLUE, libmicon.LCD_COLOR_GREEN, libmicon.LCD_COLOR_ORANGE, libmicon.LCD_COLOR_AQUA]:
 	test.set_lcd_color(color)
-#	time.sleep(0.5)
+	time.sleep(0.5)
 
 #set LCD backlight brightness
 for level in [libmicon.LCD_BRIGHT_OFF,libmicon.LCD_BRIGHT_LOW,libmicon.LCD_BRIGHT_MED,libmicon.LCD_BRIGHT_FULL]:
 	test.set_lcd_brightness(level)
-#	time.sleep(0.25)
+	time.sleep(0.25)
 
 #run some standalone commands
 test.send_write_cmd(0,libmicon.lcd_disp_animation)
