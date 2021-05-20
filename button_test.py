2ae5b8a059a3b0c8d33810df8db6d649911ed616
Jeremy J. Peper <jeremy@earth>
2021-05-20 15:33:44 -0500

    adjust output to be more logical

--- button_test.py
+++ button_test.py
@@ -3,26 +3,24 @@
 import libmicon
 import time
 
-test = libmicon.micon_api("/dev/ttyS1")
-
 print("")
 print("Press Buttons on the Device and watch the ouput")
 print("Enter CTRL-C to quit")
 print("")
 
-state=test.send_read_cmd(0x36)
-oldstate=state
+oldstate=0
 while True:
+	test = libmicon.micon_api("/dev/ttyS1")
 	state=test.send_read_cmd(0x36)
 	if oldstate != state:
 		print("Button(s): ", end='')
 		for button_num in range(8):
 			if state[0] & (2**button_num) == 0:
-				print(button_num," ", end='')
-		print("")	
-	
+				print(2**button_num," ", end='')
+		print("")
+
 	oldstate=state
-	time.sleep(0.2)
+	time.sleep(0.5)
 
 test.port.close()
 quit()
